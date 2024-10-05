3ce5bf751dc84d4be55d7d195316687a73b1afec
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-10-05 09:05:09 +0000

    fin guest

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,12 @@ class Users::SessionsController < ApplicationController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to about_path, notice: 'ゲストユーザーとしてログインしました。'
   end
+  
+  def destroy
+    sign_out(current_user)
+    redirect_to root_path, notice: 'ログアウトしました。'
+  end
+  
 end
