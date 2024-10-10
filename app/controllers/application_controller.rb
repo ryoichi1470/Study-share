ee0d33e3da707e20dc09227b566abbc45a74b0f3
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-10-10 07:29:02 +0000

    1stデプロイ+テスト完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,18 @@ class ApplicationController < ActionController::Base
   end
   
   
+  
+  
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
   
+  private
+  
+  
+
+
   
 end
