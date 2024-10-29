62457db1467a7aef2078e04e0a0a5384c9d09775
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-10-29 10:33:58 +0000

    fix page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,24 +2,20 @@ class SearchesController < ApplicationController
   def search
     @query = params[:query]
     @search_type = params[:search_type]
-    page = params[:page].to_i > 0 ? params[:page].to_i : 1  
-  
+
     @users = []
     @posts = []
-  
+
     if @search_type == 'users'
-      @users = User.where("name LIKE ?", "%#{@query}%").page(page).per(10)
+      @users = User.where("name LIKE ?", "%#{@query}%").page(params[:page]).per(10)
     elsif @search_type == 'posts'
-      @posts = Post.where("title LIKE ? OR text LIKE ?", "%#{@query}%", "%#{@query}%").page(page).per(10)
+      @posts = Post.where("title LIKE ? OR text LIKE ?", "%#{@query}%", "%#{@query}%").page(params[:page]).per(10)
     else
-      @users = User.all.page(page).per(10)
-      @posts = Post.all.page(page).per(10)
+      @users = User.page(params[:page]).per(10)
+      @posts = Post.page(params[:page]).per(10)
     end
-  
+
     @total_users = User.count
     @total_posts = Post.count
-  
-    @current_page = page
   end
-
 end
